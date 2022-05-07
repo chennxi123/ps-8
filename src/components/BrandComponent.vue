0edc150187db57acec8a74b740e770fbd42697e7
chennxi123 <104598652+chennxi123@users.noreply.github.com>
2022-05-07 13:39:40 -0400

    Add files via upload

---
+++ src/components/BrandComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <router-link to="/about" class="brand">
+        <picture class="brand__img">
+            <source srcset="/img/brand_2x.png 1x, /img/brand_large_2x.png 2x" media="(min-width: 120em)">
+            <img class="brand__img" srcset="/img/brand_1x.png 1x, /img/brand_2x.png 2x" alt="Martin Zane Brand Image">
+        </picture>
+    
+        
+    </router-link>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+    .brand
+    {
+        width: 10rem;
+        height: 6rem;
+        display: block;
+
+        @media only screen and (min-width: 100em)
+        {
+            width: 15rem;
+            height: 9rem;
+        }
+        
+        &__img
+        {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+</style>
